c359537505189ffea71366a11b36522ad9881383
Nir Pikan <nirpikk@gmail.com>
2020-10-31 20:34:41 +0200

    Next Level - levels 1-3

---
+++ Bestest game/Assets/Scripts/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public static int _nextLevelIndex = 1;
+
+    public void NextLevel()
+    {
+        _nextLevelIndex++;
+        if(_nextLevelIndex < 10)
+        {
+            SceneManager.LoadScene("Level_" + _nextLevelIndex);
+            //FindObjectOfType<AudioManager>().StopPlayingEverything();
+            //FindObjectOfType<AudioManager>().Play("Theme" + _nextLevelIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene("Victory");
+            //FindObjectOfType<AudioManager>().StopPlayingEverything();
+            //FindObjectOfType<AudioManager>().Play("Victory");
+        }
+
+    }
+}
