0949c4ff12d5d80c962e06cae1ce91a52cec663d
Angelb <angelb@post.bgu.ac.il>
2020-11-01 16:53:37 +0200

    final updates
    added barak's ability losing animation
    final tweaks

---
+++ Bestest game/Assets/Scripts/Brain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Brain : MonoBehaviour
+{
+    public string ability = "HELLO";
+    void OnEnable()
+    {
+        foreach (Text text in GetComponentsInChildren<Text>())
+        {
+            text.text = ability;
+        }
+    }
+
+    
+}
